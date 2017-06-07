65c5d5eb268609be03d4bffc11725ecca98894c0
Alexey Shevchenko <ivanovosity@gmail.com>
2017-06-07 17:18:32 +0300

    Add plugin sh4tinymce

---
+++ assets/plugins_pack/plugins/sh4tinymce/langs/ru.js
@@ -0,0 +1,12 @@
+tinymce.addI18n('ru',{
+	'SH4TinyMCE - Code Editor'	: 'SH4TinyMCE - Редактор кода',
+	'Insert/Edit Code'			: 'Вставить/Редактировать',
+	'Language'					: 'Язык',
+	'Auto links'				: 'Auto links',
+	'Gutter'					: 'Gutter',
+	'Html script'				: 'HTML script',
+	'Toolbar'					: 'Toolbar',
+	'Highlight'					: 'Highlight',
+	'Tab size'					: 'Tab size',
+	'First Line'				: 'First Line',
+});
